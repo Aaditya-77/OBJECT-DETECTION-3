efa3b136866a9873886fafba5471a75b85a592e5
Aaditya-77 <103941616+Aaditya-77@users.noreply.github.com>
2022-09-16 14:12:04 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,52 @@
+var status="";
+objects = [];
+function setup(){
+    canvas = createCanvas(650,450);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modalloaded);
+    document.getElementById("status").innerHTML="status = detecting objects";
+
+}
+
+function modalloaded(){
+    console.log("Modal is loaded");
+    Status=true;
+    objectDetector.detect(img,gotresults);
+}
+
+
+function gotresults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+img="";
+
+function preload(){
+    img = loadImage('0x0.jpg');
+}
+
+function draw(){
+    image(img,0,0,600,450);
+    if(Status!="" ){
+        objectDetector.detect(img,gotresults);
+        for(var i=0;i<objects.length;i++){
+    
+            document.getElementById("status").innerHTML="status : object detected";
+            document.getElementById("tname").innerHTML="number of objects =" + objects.length;
+            fill("blue");
+            stroke("darkgreen");
+            var percent=objects[0].confidence*100;
+            percent=floor(percent);
+            text(objects[i].label+"  "+ percent + "   % " +  " ",objects[i].x,objects[i].y);
+            noFill();
+            stroke("darkblue");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+        }
+   
+   
+}
+
---
+++ main.js
@@ -0,0 +1,24 @@
+function umbrella(){
+    window.location="umbrella.html";
+    
+}
+
+function car(){
+    window.location="car.html";
+    
+}
+
+function flower(){
+    window.location="flower.html";
+    
+}
+
+function ball(){
+    window.location="ball.html";
+    
+}
+
+function pencil(){
+    window.location="pencil.html";
+    
+}
